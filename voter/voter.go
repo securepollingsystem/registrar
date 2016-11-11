8610c32a27d819a5ac679bb64a8aafdf777f187a
Blake Griffith <cowlicks@riseup.net>
2016-11-11 11:48:00 -0800

    notes on voter implementation

--- voter/voter.go
+++ voter/voter.go
@@ -1,3 +1,7 @@
+/* TODO:
+implement stuff for one registrar
+then make it work multiple
+*/
 package voter
 
 import (
@@ -8,7 +12,10 @@ import (
 type Voter struct {
 	privateKey *ecdsa.PrivateKey
 	PublicKey  *ecdsa.PublicKey
+	// these will eventually be per registrar
 	sig        *blind.BlindSignature
+	//registrar signer's publickey
+	//registration session info from blinding
 }
 
 func NewVoter() *Voter {
@@ -16,3 +23,13 @@ func NewVoter() *Voter {
 	return &Voter{privateKey: keys,
 		PublicKey: &keys.PublicKey}
 }
+
+/*
+// get response from signer.BlindSession, return blinded v.PublicKey
+func (v * Voter) RequestRegistration(registrar * blind.Registrar) (blindedmessage *big.Int) {
+}
+
+// get sig from signer, unblind and store it
+func (v *Voter) Register(blindsig) err {
+}
+*/
