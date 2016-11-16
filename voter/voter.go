b01730e8ad522a78eee61c8e0ce2dadc152dafc6
Blake Griffith <cowlicks@riseup.net>
2016-11-16 12:50:55 -0800

    Add voter registration.

--- voter/voter.go
+++ voter/voter.go
@@ -6,14 +6,19 @@ package voter
 
 import (
 	"crypto/ecdsa"
+	"crypto/sha256"
+	"encoding/json"
 	"github.com/securepollingsystem/registrar/blind"
+	"math/big"
 )
 
 type Voter struct {
 	privateKey *ecdsa.PrivateKey
 	PublicKey  *ecdsa.PublicKey
+	pubhash    *big.Int
 	// these will eventually be per registrar
-	sig        *blind.BlindSignature
+	sig       *blind.BlindSignature
+	requester *blind.BlindRequester
 	//registrar signer's publickey
 	//registration session info from blinding
 }
@@ -24,12 +29,20 @@ func NewVoter() *Voter {
 		PublicKey: &keys.PublicKey}
 }
 
-/*
 // get response from signer.BlindSession, return blinded v.PublicKey
-func (v * Voter) RequestRegistration(registrar * blind.Registrar) (blindedmessage *big.Int) {
+func (v *Voter) RequestRegistration(pub, session *ecdsa.PublicKey) (blinded *big.Int) {
+	key, _ := json.Marshal(v.PublicKey)
+	hasher := sha256.New()
+	hasher.Write(key)
+	hashed := hasher.Sum(nil)
+	v.pubhash = new(big.Int).SetBytes(hashed)
+
+	v.requester, _ = blind.NewRequest(pub, session, v.pubhash)
+	return v.requester.Mhat
 }
 
 // get sig from signer, unblind and store it
-func (v *Voter) Register(blindsig) err {
+func (v *Voter) Register(blindsig *big.Int) {
+	v.sig = v.requester.BlindExtract(blindsig)
+	v.sig.M = v.pubhash
 }
-*/
--- voter/voter_test.go
+++ voter/voter_test.go
@@ -1,9 +1,21 @@
 package voter
 
 import (
+	"fmt"
+	"github.com/securepollingsystem/registrar/blind"
 	"testing"
 )
 
 func TestVoter(t *testing.T) {
-	NewVoter()
+	signer := blind.NewSigner()
+	pub, session, _ := signer.BlindSession()
+
+	voter := NewVoter()
+	message := voter.RequestRegistration(pub, session)
+	sig, err := signer.BlindSign(message, *session)
+	if err != nil {
+		t.Fatal()
+	}
+	voter.Register(sig)
+	fmt.Println(sig)
 }
