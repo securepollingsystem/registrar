35e887a7d7db049647bfc2ed3a49dceb7912a928
Blake Griffith <cowlicks@riseup.net>
2018-07-23 15:35:44 -0700

    Use Pollee.NewBlindSession in tests

--- blind/blind_test.go
+++ blind/blind_test.go
@@ -1,16 +1,57 @@
 package blind
+/*
+* Refactor NewBlindRequest to always blind its own public key
+* Is it okay to blind multiple things with one public key?
+* Create a `session` struct that is the registrar's pub key and the session
+*/
 
 import (
 	"math/big"
 	"testing"
+//	"fmt"
+//	"encoding/json"
 )
+
+/*
+func TestTwo(t *testing.T) {
+	// Create a registrar
+	registrar := NewRegistrar()
+
+	// Create a pollee
+	pollee := NewPollee()
+
+	// Registrar creates a new blinding session for pollee
+	session, err := registrar.BlindSession()
+	if err != nil {
+		t.Fatal()
+	}
+
+	// Pollee blinds it's public key for the registrar
+	polleeBlindedPubKey := pollee.BlindPublicKey(session)
+
+	// Registrar signs the pollee's blinded public key
+	polleeBlindedPubKeySig, err := registrar.BlindSign(polleeBlindedPubKey, *session)
+
+	polleePubKeySig := pollee.ExtractAndSaveSignature(session, polleeBlindedPubKeySig)
+
+	m := new(big.Int).SetBytes([]byte("this is the message to sign"))
+	mSig := pollee.Sign(session, m)
+
+	if !VerifySig(m, mSig, polleePubKey, polleeBlindedPubKeySig, session.registrarPubKey) {
+		t.Fatal()
+	}
+}
+*/
 
 func TestMain(t *testing.T) {
 	// Create a registrar
 	registrar := NewRegistrar()
 
-	// Pollee's message that needs to be blind-signed
-	m := new(big.Int).SetBytes([]byte("this is the message to blind-sign"))
+	// Create a pollee
+	pollee, err := NewPollee()
+	if err != nil {
+		t.Fatal()
+	}
 
 	// Pollee gets the registrar's session key and publick key
 	session, err := registrar.NewBlindSession()
@@ -20,7 +61,7 @@ func TestMain(t *testing.T) {
 
 	// Pollee blinds her message
 	// nb: the blinded message is the Mhat field on the request
-	request, err := NewBlindRequest(session, m)
+	request, err := pollee.NewBlindRequest(session)
 	if err != nil {
 		t.Fatal()
 	}
@@ -35,7 +76,7 @@ func TestMain(t *testing.T) {
 	sig := request.BlindExtract(sHat)
 
 	// Onlooker verifies signature
-	sig.M = m
+	sig.M = new(big.Int).SetBytes(MarshalPublicKey(pollee.PublicKey))
 	if !BlindVerify(session.Q, sig) {
 		t.Fatal("valid signature\n")
 	}
