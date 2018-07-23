0c992b8324fbde20c4b917e1bc4e93ace2e1ae3a
Blake Griffith <cowlicks@riseup.net>
2018-07-23 15:29:45 -0700

    Add funcs to Un/Marshal ec pubkeys

--- blind/keys.go
+++ blind/keys.go
@@ -3,11 +3,22 @@ package blind
 import (
 	"crypto/ecdsa"
 	"crypto/rand"
+	"crypto/elliptic"
 )
 
 // GenerateKey generates a public and private key pair
 func GenerateKey() (*ecdsa.PrivateKey, error) {
 	return ecdsa.GenerateKey(Secp256k1(), rand.Reader)
+}
+
+func MarshalPublicKey(pk *ecdsa.PublicKey) []byte {
+	return elliptic.Marshal(Secp256k1(), pk.X, pk.Y)
+}
+
+func UnmarshalPublicKey(b []byte) *ecdsa.PublicKey {
+	Curve := Secp256k1()
+	X, Y := elliptic.Unmarshal(Curve, b)
+	return &ecdsa.PublicKey{Curve, X, Y}
 }
 
 func KeysEqual(a, b *ecdsa.PublicKey) bool {
