f2d1babf7cc93e29222df296285183706d34c792
Blake Griffith <cowlicks@riseup.net>
2018-08-17 15:21:59 -0700

    clean up blind_test.go and add comments

--- blind/blind_test.go
+++ blind/blind_test.go
@@ -9,37 +9,6 @@ import (
 	"testing"
 )
 
-/*
-func TestTwo(t *testing.T) {
-	// Create a registrar
-	registrar := NewRegistrar()
-
-	// Create a pollee
-	pollee := NewPollee()
-
-	// Registrar creates a new blinding session for pollee
-	session, err := registrar.BlindSession()
-	if err != nil {
-		t.Fatal()
-	}
-
-	// Pollee blinds it's public key for the registrar
-	polleeBlindedPubKey := pollee.BlindPublicKey(session)
-
-	// Registrar signs the pollee's blinded public key
-	polleeBlindedPubKeySig, err := registrar.BlindSign(polleeBlindedPubKey, *session)
-
-	polleePubKeySig := pollee.ExtractAndSaveSignature(session, polleeBlindedPubKeySig)
-
-	m := new(big.Int).SetBytes([]byte("this is the message to sign"))
-	mSig := pollee.Sign(session, m)
-
-	if !VerifySig(m, mSig, polleePubKey, polleeBlindedPubKeySig, session.registrarPubKey) {
-		t.Fatal()
-	}
-}
-*/
-
 func TestMain(t *testing.T) {
 	// Create a registrar
 	registrar := NewRegistrar()
@@ -64,20 +33,24 @@ func TestMain(t *testing.T) {
 	}
 
 	// Registrar signs the message
-	sHat, err := registrar.BlindSign(request.Mhat, *session.R)
+	blindedSignature, err := registrar.BlindSign(request.Mhat, *session.R)
 	if err != nil {
 		t.Fatal()
 	}
 
-	if !pollee.BlindExtract(request, sHat) {
+	// Pollee extracts the blinded signature and stores it
+	if !pollee.BlindExtract(request, blindedSignature) {
 		t.Fatal("invalid signature");
 	}
 
+	// Pollee signs some message
 	msg := []byte("This is a message I want to sign.");
 	sig, err := pollee.Sign(msg);
 	if err != nil {
 		t.Fatal("signing error");
 	}
+
+	// Pollee verifies the signature to the message
 	if !pollee.Verify(msg, sig) {
 		t.Fatal("verification failed");
 	}
